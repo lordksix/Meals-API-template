99830049e012744c0a76747b4c2f7193bc45f527
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-22 22:31:59 -0500

    Add categories and event listener

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './style.css';
 import './asset/resource/icons.svg';
+import {
+  printList,
+} from './modules/utils.js';
+
+const navItem = document.querySelectorAll('.nav-items a');
+
+// Select section to show
+navItem.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    link.classList.add('active');
+    printList(e);
+  });
+});
