1a13ef69daf2e695d00aee79cb501f6e196b6101
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-23 05:19:25 -0500

    Add Involvement API interactivity

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -2,11 +2,11 @@ import {
   createApiMealURL, createApiInvURL,
 } from './createURLAPI.js';
 import { createItem } from './ListItemMod.js';
-import { handleScoreFormSubmit } from './PostForm.js';
+import { handlePost } from './PostAPI.js';
 import { handleGETAPI } from './GetAPI.js';
 import {
   classes, baseurlFilterMeal, baseurllookupMeal, parameterMealID, parameterMealCat,
-  xlink, baseurlInvolvement, parameterLikeApp, parameterCommApp, parameterReservApp,
+  xlink, baseurlInvolvement, parameterLikeApp,
   parameterIDApp,
 } from './const.js';
 
@@ -15,8 +15,30 @@ const appendResponsePara = async (node) => {
   showResponseForm.textContent = node;
 };
 
-const findLikes = async (mealData, invArr) => {
-  console.log(mealData[0].idMeal);
+const likesJson = async (id) => (
+  {
+    item_id: id,
+  }
+);
+
+const findLikes = async (id, invArr) => {
+  const result = invArr.filter((invArr) => invArr.item_id === id);
+  if (result.length > 0) return result[0].likes;
+  return 0;
+};
+
+const createLike = async (event) => {
+  if (event.target.classList.contains('heart-like')) {
+    const currentID = event.currentTarget.dataset.id;
+    const url = createApiInvURL(baseurlInvolvement, parameterIDApp, parameterLikeApp);
+    await handlePost(url, await likesJson(currentID));
+    const dataResponseInv = await handleGETAPI(url);
+    const qtyLikes = await findLikes(currentID, dataResponseInv);
+    const likeText = event.target.parentNode.parentNode.querySelector('.app-like-text');
+    likeText.textContent = `${qtyLikes} like${qtyLikes > 1 ? 's' : ''}`;
+    const useVSG = event.target.querySelector('use');
+    useVSG.setAttribute('href', '../asset/resource/icons.svg#heart-like-red');
+  }
 };
 
 const getMealsAPIResponse = async (data) => {
@@ -30,7 +52,7 @@ const appendItems = async (dataArr, invArr) => {
   appCtn.innerHTML = '';
   dataArr.forEach(async (data) => {
     const mealData = await getMealsAPIResponse(data);
-    const mealFrag = await createItem('li', classes, mealData, xlink, await findLikes(mealData, invArr));
+    const mealFrag = await createItem('li', classes, mealData, xlink, await findLikes(mealData[0].idMeal, invArr), createLike);
     appCtn.appendChild(mealFrag);
   });
 };
