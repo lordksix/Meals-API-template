30470cbf46d5077f5d2989d804a1daed23863bc8
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-24 12:39:02 -0500

    Add counter to utils module

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -2,6 +2,7 @@ import { createItem } from './ListItemMod.js';
 import { classes, xlink } from './const.js';
 import { createLike, findLikes, getLikesResponse } from './APILikeHandling.js';
 import { getMealsIDResponse, getMealsCatResponse } from './APIMealsHandling.js';
+import { countTotalItem, appendTotal } from './CountTotalItems.js';
 
 const appendResponsePara = async (node) => {
   const showResponseForm = document.getElementById('formFeedback');
@@ -13,13 +14,15 @@ const getCat = async (event) => {
   return catID;
 };
 
-const appendItems = async (dataArr, invArr) => {
+const appendItems = async (dataArr, invArr, ID) => {
   const appCtn = document.getElementById('app-ctn');
   appCtn.innerHTML = '';
   dataArr.forEach(async (data) => {
     const mealData = await getMealsIDResponse(data.idMeal);
     const mealFrag = await createItem('li', classes, mealData.meals, xlink, await findLikes(mealData.meals[0].idMeal, invArr), createLike);
     appCtn.appendChild(mealFrag);
+    const total = await countTotalItem(appCtn);
+    appendTotal(total, ID);
   });
 };
 
@@ -28,7 +31,7 @@ const printList = async (event) => {
   const dataResponseMeal = await getMealsCatResponse(catID);
   const dataResponseInv = await getLikesResponse();
   if (dataResponseMeal instanceof Error || dataResponseInv instanceof Error) appendResponsePara('Unable to Fetch Data');
-  else await appendItems(dataResponseMeal.meals, dataResponseInv);
+  else await appendItems(dataResponseMeal.meals, dataResponseInv, catID);
 };
 
 export {
