81795c21df847e0fe4be5ce8ad43100952d5f5c6
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-24 12:35:05 -0500

    Add Elements Counter

--- src/modules/CountTotalItems.js
+++ src/modules/CountTotalItems.js
@@ -1,1 +1,10 @@
-export const countTotalItem = (node) => node.children.length;
+const countTotalItem = async (node) => node.children.length;
+
+const appendTotal = async (total, id) => {
+  const idSpan = document.getElementById(id);
+  idSpan.textContent = ` (${total})`;
+};
+
+export {
+  countTotalItem, appendTotal,
+};
