2462d3f95a291b4267054baf81e3b12c067a88d3
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-25 02:46:27 -0500

    Add edge tests

--- __tests__/CountTotalItems.test.js
+++ __tests__/CountTotalItems.test.js
@@ -6,7 +6,22 @@ import { countTotalItem } from '../src/modules/CountTotalItems.js';
 
 describe('Button Generator Function', () => {
   const wapper = document.createElement('div');
+  const inner = document.createElement('div');
+  const inner2 = document.createElement('div');
+
   test('No inner Child', () => {
     expect(countTotalItem(wapper)).toBe(0);
   });
+
+  test('One Inner Child', () => {
+    wapper.appendChild(inner);
+    expect(countTotalItem(wapper)).toBe(1);
+  });
+
+  test('One Nested Inner Child', () => {
+    inner.appendChild(inner2);
+    wapper.appendChild(inner);
+    expect(countTotalItem(wapper)).toBe(1);
+  });
+
 });
