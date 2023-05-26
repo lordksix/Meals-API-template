f0267930551563792d32b85e9aa69faa13eef60b
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-26 08:45:02 -0500

    Add Get URL generator for Involvemnt AP

--- src/modules/createURLAPI.js
+++ src/modules/createURLAPI.js
@@ -3,11 +3,16 @@ const createApiMealURL = (baseurl, queryKey, queryValue) => {
   return url;
 };
 
-const createApiInvURL = (baseurl, id = false, endpoint = false) => {
+const createApiInvURL = (baseurl, id, endpoint) => {
   const url = `${baseurl}${id}${endpoint}`;
   return url;
 };
 
+const createApiInvQueryURL = (baseurl, id, endpoint, query) => {
+  const url = `${baseurl}${id}${endpoint}?item_id=${query}`;
+  return url;
+};
+
 export {
-  createApiMealURL, createApiInvURL,
+  createApiMealURL, createApiInvURL, createApiInvQueryURL,
 };
