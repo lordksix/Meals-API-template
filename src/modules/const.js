5f35ef7a0ef6de4c72fcb3fcdad584bfbf95d8a7
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-26 08:29:28 -0500

    Create object with classes name for sections

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,34 +1,67 @@
-const classCtn = 'app-item';
-const classImg = 'app-img';
-const classTittle = 'app-title';
-const classLikes = 'app-like';
-const classLikesSVG = 'app-like-svg';
-const classLikesText = 'app-like-text';
-const classDivBtn = 'app-div-btn';
-const classBtnComment = ['app-btn', 'comment'];
-const classBtnReservation = ['app-btn', 'reservation'];
+const itemClasses = {
+  container: 'app-item',
+  image: 'app-img',
+  titleHeader: 'app-title-div',
+  likesCtn: 'app-like',
+  likesIcon: 'app-like-svg',
+  likesText: 'app-like-text',
+  btnCtn: 'app-div-btn',
+  btnComment: ['app-btn', 'comment'],
+  btnReserve: ['app-btn', 'reservation'],
+};
 
-const classes = [
-  classCtn, classImg, classTittle, classLikes,
-  classLikesSVG, classLikesText, classDivBtn, classBtnComment,
-  classBtnReservation,
-];
+const popUpClasses = {
+  container: 'app-item-popup',
+  image: 'app-img-pop',
+  titleHeader: 'app-title-header',
+  properties: 'app-property',
+  property: 'app-prop',
+  exit: 'app-exit',
+  overlay: 'app-item-popup-overlay',
+  title: 'app-title-pop',
+  sectionCtn: 'app-sec-ctn',
+  formContainer: 'app-form-sec-ctn',
+};
+
+const popUpSectionClasses = {
+  container: 'app-sec-ctn',
+  titleheader: 'app-section-header',
+  title: 'app-section-title',
+  titleCounter: ['app-section-title', 'app-section-counter'],
+  postCtn: 'app-section-ctn',
+  postItem: 'app-section-item',
+};
+
+const popUpFormClasses = {
+  container: 'app-form-sec-ctn',
+  title: 'app-form-title',
+  formContainer: 'app-form-ctn',
+  inputHidden: 'app-form-input-hidden',
+  inputDiv: 'app-form-input-div',
+  inputLabel: 'app-form-input-label',
+  inputReg: 'app-form-input-reg',
+  textArea: 'app-form-textArea',
+  button: 'app-btn',
+};
 
 const xlink = '../asset/resource/icons.svg#heart-like';
 
-const baseurlFilterMeal = 'https://www.themealdb.com/api/json/v1/1/filter.php?';
-const baseurllookupMeal = 'https://www.themealdb.com/api/json/v1/1/lookup.php?';
-const parameterMealID = 'i';
-const parameterMealCat = 'c';
+const meaAPIUri = {
+  baseFilter: 'https://www.themealdb.com/api/json/v1/1/filter.php?',
+  baseLookUp: 'https://www.themealdb.com/api/json/v1/1/lookup.php?',
+  queryLookUp: 'i',
+  queryFilter: 'c',
+};
 
-const baseurlInvolvement = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-const parameterLikeApp = 'likes/';
-const parameterCommApp = 'comments/';
-const parameterReservApp = 'reservations/';
-const parameterIDApp = 'Wbe3IznQ2LQoFPHmVrwp/';
+const involvementAPIUri = {
+  base: 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/',
+  queryLike: 'likes/',
+  queryComment: 'comments',
+  queryReserve: 'reservations',
+  appID: 'Wbe3IznQ2LQoFPHmVrwp',
+};
 
 export {
-  classes, baseurlFilterMeal, baseurllookupMeal, parameterMealID, parameterMealCat,
-  xlink, baseurlInvolvement, parameterLikeApp, parameterCommApp, parameterReservApp,
-  parameterIDApp,
+  xlink, popUpSectionClasses, itemClasses, popUpClasses, popUpFormClasses,
+  meaAPIUri, involvementAPIUri,
 };
